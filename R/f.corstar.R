f2f56167935cffc965caa44e868b25859d6968c4
Adam Meade <awmeade.mail@gmail.com>
2016-04-02 12:03:57 -0400

    initial commit

---
+++ R/f.corstar.R
@@ -0,0 +1,55 @@
+#' Add asterisk(s) to significant correlations 
+#'
+#' Uses an output object from the Psych package and adds 1 or 2 askterisks (stars) to the 
+#' correlation for APA table output. Returns object as dataframe.
+#'
+#'
+#' @param corr.obj is the output list object from the corr.test function in the Psych package
+#' @param is.triangle is whether the correlation is a symetric matrix (default) or a rectangular 
+#' matrix in which one set of variables is correlated with a different set.
+#' @param p.val.1 is the p-value desired to trigger a single astrisk (default = .05)
+#' @param p.val.2 is the p-value desired to trigger a second astrisk added to the first
+#' default = .01. p.val.2 should be a smaller p val than p.val.1. set p.val.2 = FALSE if
+#' no second astrisk is desired
+#'
+#' @author Adam Meade \email{awmeade@@ncsu.edu}
+#' @export 
+#' @examples
+#' library('psych')
+#' R = matrix(cbind(1,.80,.2,.80,1,.7,.2,.7,1),nrow=3)
+#' U = t(chol(R))
+#' set.seed(1)
+#' random.normal = matrix(rnorm(dim(U)[1]*100,0,1), nrow=dim(U)[1], ncol=100);
+#' X = as.data.frame(t(U %*% random.normal))
+#' corrs.1 = corr.test(X)
+#' f.corstar(corrs.1)
+#' f.corstar(corrs.1, p.val.1 = .01, p.val.2 = FALSE)
+#' corrs.2 = corr.test(X[1:2],X[3])
+#' f.corstar(corrs.2,is.triangle = FALSE)
+
+f.corstar <- local(function(corr.obj, is.triangle = TRUE, p.val.1 = .05, p.val.2 = .01){
+  R <- corr.obj$r
+  p <- corr.obj$p
+  ## define notions for significance levels; spacing is important.
+  if(p.val.2){
+    mystars <- ifelse(p < p.val.1,ifelse(p<p.val.2,"** ","* "), " ")
+  }else{
+    mystars <- ifelse(p < p.val.1,"* ", " ")
+  }
+  ## trunctuate the matrix that holds the correlations to two decimal
+  R <- format(round(R, 2))
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(R))
+  rownames(Rnew) <- rownames(R)
+  colnames(Rnew) <- paste(colnames(R), "", sep="")
+  ## if triangle, remove upper triangle
+  if(is.triangle==TRUE){
+    diag(Rnew) <- paste(diag(R), " ", sep="")
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+    ## remove last column and return the matrix (which is now a data frame)
+    Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  }
+  return(Rnew)
+}) 
---
+++ R/r.write.corrs.R
@@ -0,0 +1,42 @@
+#' Formats in APA format and writes correlation matrices to an excel sheet
+#'
+#' Uses an output object from the Psych package and adds 1 or 2 askterisks (stars) to the 
+#' correlation for APA table output. Returns object as dataframe. Writes correlational 
+#' data for r, n, and pvalues to three consecutively named Excel sheets.
+#'
+#'
+#' @param f.name is the file name to which to write the data. default = 'output.xlsx'
+#' @param s.name is the name of the Excel sheet(s) to which to write the data
+#' @param obj.corrs is the correlation object from the corr.test function in the Psych package
+#' @param print.p requests printing of p values. default = TRUE
+#' @param print.n requests printing of sample size (n). default = TRUE
+#' @param ... parameters passed to the f.starcor function
+#' 
+#' @author Adam Meade \email{awmeade@@ncsu.edu}
+#' @export 
+#' @examples
+#' library('psych')
+#' R = matrix(cbind(1,.80,.2,.80,1,.7,.2,.7,1),nrow=3)
+#' U = t(chol(R))
+#' set.seed(1)
+#' random.normal = matrix(rnorm(dim(U)[1]*100,0,1), nrow=dim(U)[1], ncol=100);
+#' X = as.data.frame(t(U %*% random.normal))
+#' corrs = corr.test(X)
+#' f.write.corrs(s.name='example',obj.corrs=corrs)
+#' f.write.corrs(s.name='example',obj.corrs=corrs,p.val.2=FALSE)
+
+f.write.corrs <- local(function(f.name = 'output.xlsx', s.name, obj.corrs, 
+                                print.p = TRUE, print.n = TRUE, ...){
+  require('xlsx')
+  corr.sheet <- paste0(s.name,'.r')
+  n.sheet <- paste0(s.name,'.n')
+  p.sheet <- paste0(s.name,'.p')
+  write.xlsx(f.corstar(obj.corrs,...), f.name, sheetName = corr.sheet, append=TRUE)
+  if(print.n == TRUE){
+    write.xlsx(obj.corrs$n,f.name, sheetName = n.sheet, append=TRUE)
+  }
+  if(print.p == TRUE){
+    write.xlsx(round(obj.corrs$p,2),f.name, sheetName = p.sheet, append=TRUE)
+  }
+  return(TRUE)
+})
