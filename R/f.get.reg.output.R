2d0497f79b2d2f4b663f1124d91771d9d7d8af38
Adam Meade <awmeade.mail@gmail.com>
2016-04-21 15:38:25 -0400

    update get reg output to remove model column

--- R/f.get.reg.output.R
+++ R/f.get.reg.output.R
@@ -14,7 +14,7 @@
 #' }
 f.get.reg.output <- local(function(out.lm){
   the.model <- summary(out.lm)$call
-  get.model <- toString(the.model)
+  #get.model <- toString(the.model)
   coef <- round(summary(out.lm)$coefficients,2)
   f <- as.matrix(round(summary(out.lm)$fstatistic,2))
   p <- round(pf(f[1],f[2],f[3],lower.tail=F),3)
@@ -26,6 +26,6 @@ f.get.reg.output <- local(function(out.lm){
   x[,1] <- f.stats
   row.names(x) <- c("F value","df1","df2","p","r","r-sq","adj.r-sq")
   return.me <- rbind(coef,x)
-  return.me <- cbind(return.me,get.model)
+  #return.me <- cbind(return.me,get.model)
   return(return.me)
 })
